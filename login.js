771c58c0c37c7e561f441e4bea618ad93c092910
denisostoich <80658827+denisostoich@users.noreply.github.com>
2021-04-05 18:02:50 -0300

    Corrections

--- login.js
+++ login.js
@@ -1,43 +1,66 @@
 var validation = document.querySelector('validation');
+var list = document.querySelector('List')
 
 var fieldsQuantity = function () {
+    validation.style.display = 'flex';
     var fields = document.querySelectorAll('input');
     var fieldsNumber = fields.length;
     if (fieldsNumber = 2) {
-        validation.append('Input fields are correct');
+        var fieldsNode = document.createElement("LI");
+        var fieldsText = document.createTextNode('Input fields are correct');
+        fieldsNode.appendChild(fieldsText);
+        list.appendChild(fieldsNode);
     }
     else {
-        validation.append('There are missing inputs');
+        var nFieldsNode = document.createElement("LI");
+        var nFieldsText = document.createTextNode('There are missing inputs');
+        nFieldsNode.appendChild(nFieldsText);
+        list.appendChild(nFieldsNode)
     }
 }
 fieldsQuantity()
 
 var validAnchor = function () {
+    validation.style.display = 'flex';
     var anchor = document.querySelector('a').href;
     var validHref = 'register.html';
     if (anchor = validHref) {
-        validation.append('Anchor tag is valid');
+        var anchorNode = document.createElement("LI");
+        var anchorText = document.createTextNode('Anchor tag is valid');
+        anchorNode.appendChild(anchorText);
+        list.appendChild(anchorNode);
     }
     else {
-        validation.append('Anchor tag is invalid');
+        var nAnchorNode = document.createElement("LI");
+        var nAnchorText = document.createTextNode('Anchor tag is invalid');
+        nAnchorNode.appendChild(nAnchorText);
+        list.appendChild(nAnchorNode);
     }
 }
 validAnchor();
 
 var validBtns = function() {
+    validation.style.display = 'flex';
     var submitBtn = document.querySelector('submit').nodeValue;
     var submitValue = 'Iniciar Sesión';
     if (submitBtn = submitValue) {
-        validation.append('Button content is correct');
+        var btnsNode = document.createElement("LI");
+        var btnsText = document.createTextNode('Buttons content is correct');
+        btnsNode.appendChild(btnsText);
+        list.appendChild(btnsNode);
     }
     else {
-        validation.append('Button content is wrong');
+        var nBtnsNode = document.createElement("LI");
+        var nBtnsText = document.createTextNode('Buttons content is wrong');
+        btnsNode.appendChild(nBtnsText);
+        list.appendChild(nBtnsNode);
     }
 }
 validBtns();
 
 var validation = function () {
     if ((fieldsQuantity = true) && (validAnchor = true) && (validBtns = true)) {
+        validation.style.display = 'flex';
         validation.textContent = 'Every validation has passed';
     }
 }
